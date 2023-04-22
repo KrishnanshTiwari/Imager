5bfc6f98cf615bb4d19846db317c56de41d3a176
KrishnanshTiwari <128737843+KrishnanshTiwari@users.noreply.github.com>
2023-04-22 22:03:21 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -94,7 +94,7 @@ const CreatePost = () => {
             value={form.prompt}
             handleChange={handleChange}
           />
-          <div className="relative bg-[#daf3ea] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+          <div className="mt-5 relative bg-[#daf3ea] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             { form.photo ? (
               <img
                 src={form.photo}
