51099a6927e1cfa9f53145432dfd35c526b2d294
KrishnanshTiwari <128737843+KrishnanshTiwari@users.noreply.github.com>
2023-06-06 15:05:51 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -33,7 +33,7 @@ const CreatePost = () => {
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (err) {
-        alert(API expired! sorry for the inconvenience);
+        alert("API expired! sorry for the inconvenience");
       } finally {
         setGeneratingImg(false);
       }
